1a50e278c59ef2aa6a9aebd72bbea1a0154768b5
Julien D. <juliendidier83550@gmail.com>
2025-12-25 00:29:11 +0100

    Style when no folder

--- front/app/upload/upload.tsx
+++ front/app/upload/upload.tsx
@@ -110,6 +110,8 @@ export default function UploadForm() {
             />
 
             <button
+                id={"startImportBtn"}
+                disabled={outputPath == null || outputPath === ""}
                 onClick={() => fileInputRef.current?.click()}
                 className="px-4 py-2 bg-blue-500 text-white rounded"
             >
